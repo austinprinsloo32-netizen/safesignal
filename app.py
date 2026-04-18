6bf4d2bd3faf8a47fa688fa3c8da86ae833ce29e
Austin Prinsloo <Austinprinsloo32@gmail.com>
2026-04-18 22:48:33 +0200

    Restructure project for deployment (templates + static)

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask, request, jsonify
-from detector import analyze_text
+from flask import Flask, request, jsonify, render_template
 from flask_cors import CORS
+from detector import analyze_text
 
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 @app.route("/analyze", methods=["POST"])
 def analyze():
