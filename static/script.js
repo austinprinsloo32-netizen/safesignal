a50b4cf65023fc58f7dfdbfd9ee5cf6d7c8eb2ee
Austin Prinsloo <Austinprinsloo32@gmail.com>
2026-04-19 16:02:38 +0200

    Fix deployed API endpoint

--- static/script.js
+++ static/script.js
@@ -100,13 +100,13 @@ async function checkScam() {
     `;
 
     try {
-        const response = await fetch("http://127.0.0.1:5000/analyze", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ text })
-        });
+    const response = await fetch("/analyze", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ text })
+});
 
         if (!response.ok) {
             throw new Error("Server returned an error.");
